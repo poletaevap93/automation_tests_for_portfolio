c7e79323013a26f0a9b5f27cfaeac9ba8b1a7814
Andrey Poletaev <poletaevap93@mail.ru>
2023-02-19 20:30:50 +0500

    update elements tests, added test web table, adding a new person to the table

--- generator/generator.py
+++ generator/generator.py
@@ -1,4 +1,6 @@
-#
+# класс для генерации рандомных значений
+import random
+
 from data.data import Person
 from faker import Faker   # импортирую библиотеку для рандомного заполнения полей в методе ниже
 
@@ -8,6 +10,11 @@ Faker.seed()
 def generated_person():
     yield Person(   # yield - это обязательная конструкция, она возвращает класс указанный   # Person - вызываю этот класс со всеми полями
         full_name=faker_ru.first_name() + " " + faker_ru.last_name() + " " + faker_ru.middle_name(),
+        firstname=faker_ru.first_name(),
+        lastname=faker_ru.last_name(),
+        age=random.randint(10,80),
+        department=faker_ru.job(),   # какая то работа рандомная подставляется
+        salary=random.randint(30000,50000),
         email= faker_ru.email(),
         current_address= faker_ru.address(),
         permanent_address= faker_ru.address(),
