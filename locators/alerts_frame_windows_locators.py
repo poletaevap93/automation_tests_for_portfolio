405e08506bf961a23a4fc2b74e9f10a38fcb1332
Andrey Poletaev <poletaevap93@mail.ru>
2023-04-06 23:04:55 +0500

    added class:    TestAlertsPage
    added test:
    - test_see_alert
    - test_alert_appear_5_sec
    - test_confirm_alert
    - test_pomt_alert

--- locators/alerts_frame_windows_locators.py
+++ locators/alerts_frame_windows_locators.py
@@ -6,3 +6,16 @@ class BrowserWindowsPageLocators:
     NEW_WINDOW_BUTTON = (By.CSS_SELECTOR, "button[id='windowButton']")
 
     TITLE_NEW = (By.CSS_SELECTOR, "h1[id='sampleHeading']")
+
+class AlertsPageLocators:
+    SEE_ALERT_BUTTON = (By.CSS_SELECTOR, "button[id='alertButton']")
+    APPEAR_ALERT_AFTER_5_SEC_BUTTON = (By.CSS_SELECTOR, "button[id='timerAlertButton']")
+    CONFIRM_BOX_ALERT_BUTTON = (By.CSS_SELECTOR, "button[id='confirmButton']")
+    CONFIRM_RESULT = (By.CSS_SELECTOR, "span[id='confirmResult']")
+    PROMT_BOX_ALERT_BUTTON = (By.CSS_SELECTOR, "button[id='promtButton']")
+    PROMT_RESULT = (By.CSS_SELECTOR, "span[id='promptResult']")
+
+
+
+
+
--- tests/alerts_frame_windows_test.py
+++ tests/alerts_frame_windows_test.py
@@ -1,6 +1,6 @@
 import time
 
-from pages.alerts_frame_windows_page import BrowserWindowsPage
+from pages.alerts_frame_windows_page import BrowserWindowsPage, AlertsPage
 
 
 class TestAlertsFrameWindows:
@@ -19,4 +19,31 @@ class TestAlertsFrameWindows:
             text_result = browser_windows_page.check_opened_new_tab()  # текст, который появляется в новом окне
             assert text_result == "This is a sample page", "the window tab has not opened"
 
+    class TestAlertsPage: # тест с алертами, всплывающими окнами
 
+        def test_see_alert(self,driver):  # обычный алерт
+            alert_page = AlertsPage (driver, "https://demoqa.com/alerts")
+            alert_page.open()
+            alert_text = alert_page.check_see_alert()
+            assert alert_text == "You clicked a button"  # проверка на соответствие текста в появившемся алерте
+
+
+        def test_alert_appear_5_sec(self,driver):  # появление алерта через 5 сек
+            alert_page = AlertsPage (driver, "https://demoqa.com/alerts")
+            alert_page.open()
+            alert_text = alert_page.check_alert_appear_5_sec()
+            assert alert_text == "This alert appeared after 5 seconds"  # проверка на соответствие текста в появившемся алерте
+
+
+        def test_confirm_alert(self,driver):  # появление алерта с вариантами выбора
+            alert_page = AlertsPage (driver, "https://demoqa.com/alerts")
+            alert_page.open()
+            alert_text = alert_page.check_confirm_alert()
+            assert alert_text == "You selected Ok"  # проверка на соответствие текста в появившемся алерте
+
+
+        def test_pomt_alert(self,driver):  # появление алерта с вариантами выбора
+            alert_page = AlertsPage (driver, "https://demoqa.com/alerts")
+            alert_page.open()
+            text, alert_text = alert_page.check_prompt_alert()  # два значения, т.к. метод возвращает тоже 2 значения
+            assert text in alert_text  # проверяем, что значение text содержится в alert_text
